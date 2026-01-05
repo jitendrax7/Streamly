d72e428752d75493d3e4766dd912db39920596b2
jitendrax7 <rathourjitendrasingh472@gmail.com>
2026-01-06 01:30:34 +0530

    upolode a code

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import movies from "../Data/movies";
+import movies from "../Data/movies.js";
 import { MovieCard } from "../components/MovieCard";
 
 export const Search = () => {
